eaa1098503f575780cd9890037fe05415cc45a84
angusdq <angusdq@gmail.com>
2023-04-27 19:56:07 +1000

    Changes

--- src/components/SystemAdminHome.js
+++ src/components/SystemAdminHome.js
@@ -1,9 +1,7 @@
-function SystemAdminHome() {
+export default function SystemAdminHome() {
     return (
       <div>
         <p>"Hello System Admin page"</p>
       </div>
     );
-  }
-  
-  export default SystemAdminHome;
+}
