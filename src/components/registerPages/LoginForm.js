0b70e28a2623e4ce777ff08feb89c5b90650fb4f
angusdq <angusdq@gmail.com>
2023-05-11 16:25:00 +1000

    Connection to backend

--- src/components/registerPages/LoginForm.js
+++ src/components/registerPages/LoginForm.js
@@ -2,6 +2,7 @@ import { React } from 'react';
 import '../../App.css'
 import { Button, TextField } from '@mui/material';
 import { useNavigate } from "react-router-dom";
+import { useState } from 'react';
 
 export default function LoginForm() {
     const navigate = useNavigate();
@@ -10,6 +11,54 @@ export default function LoginForm() {
       navigate('/signupselectuser');
     };
 
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    
+    const handleUsernameChange = (event) => {
+        setUsername(event.target.value);
+    };
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    };
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        const signIn = { username, password };
+        console.log(signIn);
+        fetch(`http://localhost:8080/author/findAuthorUsername/${username}`)
+          .then((response) => {
+            if (!response.ok) {
+              alert('Incorrect Username or Password');
+              throw response;
+            }
+            return response.json();
+          })
+          .then((json) => {
+            console.log(json);
+            handleClick2(e);
+          });
+      };
+
+      const handleClick2 = (e) => {
+        e.preventDefault();
+        const signIn = { username, password };
+        console.log(signIn);
+        fetch(`http://localhost:8080/author/findAuthorPassword/${password}`)
+          .then((response) => {
+            if (!response.ok) {
+              alert('Incorrect Username or Password');
+              throw response;
+            }
+            return response.json();
+          })
+          .then((json) => {
+            console.log(json);
+            localStorage.setItem('loggedin', true);
+            alert('fuck you yes man')
+          });
+      };
+
     return(
       <>  
         <div class="signup-login">
@@ -17,9 +66,9 @@ export default function LoginForm() {
         </div>
         <div class='selectUserRegisterStyle'>        
           <p>Sign in</p>
-          <TextField id="username" type="username" placeholder="username" />
-          <TextField id="password" type="password" placeholder="password" />
-          <Button class="buttonRegisterPage" variant="contained" >Sign in</Button>
+          <TextField id="username" type="username" placeholder="username" onChange={handleUsernameChange} />
+          <TextField id="password" type="password" placeholder="password" onChange={handlePasswordChange} />
+          <Button class="buttonRegisterPage" variant="contained" onClick={handleClick}>Sign in</Button>
         </div>
       </>
     )
